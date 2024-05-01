ecf1f0860237e120e8ff7142bf4f0f4e5286175d
krsh17 <krish.kishor17@gmail.com>
2024-05-01 23:56:21 +0530

    committed

--- googlecomputecustom/Resource.tf
+++ googlecomputecustom/Resource.tf
@@ -1,5 +1,5 @@
 resource "google_compute_instance" "example_instance" {
-  name         = "example-instance1"
+  name         = "example-instance11"
   machine_type = "e2-micro"
   zone         = "us-central1-a"
  
