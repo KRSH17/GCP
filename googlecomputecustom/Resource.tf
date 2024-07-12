3c1d83aadd9ec725ddd37247ffc32370ba1011e1
KRSH17 <168670845+KRSH17@users.noreply.github.com>
2024-07-12 23:41:46 +0530

    Update Resource.tf

--- googlecomputecustom/Resource.tf
+++ googlecomputecustom/Resource.tf
@@ -14,3 +14,25 @@ resource "google_compute_instance" "example_instance" {
     access_config {}
   }
 }
+
+
+
+# Define the VPC network
+resource "google_compute_network" "example_vpc" {
+  name                    = "example-vpc"
+  auto_create_subnetworks = false  # Disable automatic subnet creation
+}
+
+# Define a subnet within the VPC network
+resource "google_compute_subnetwork" "example_subnet" {
+  name          = "example-subnet"
+  ip_cidr_range = "10.40.0.0/25"
+  region        = "us-central1"
+  network       = google_compute_network.example_vpc.name
+}
+resource "google_compute_subnetwork" "example_subnet2" {
+  name          = "example-subnettwo"
+  ip_cidr_range = "10.40.0.128/25"
+  region        = "us-central1"
+  network       = google_compute_network.example_vpc.name
+}
